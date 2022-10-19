fb25e7e44d7a7988d8b07db6277bd8045cdcf63b
Sun min Lee <sunmin.lee@pbagel.com>
2022-10-19 16:48:53 +0900

    모델 추가

--- main.py
+++ main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import numpy as np
+import pandas as pd
+import torch
+from sklearn.preprocessing import StandardScaler
+from torch import nn
 
 
-# Press the green button in the gutter to run the script.
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(150, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1),
+        )
+
+    def forward(self, x):
+        return self.linear_relu_stack(x)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # 데이터 로드
+    test_csv = pd.read_csv('input/test.csv')
+    train_csv = pd.read_csv('input/train.csv')
+    y = train_csv['SalePrice']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(train_csv.tail(5))
+    print(test_csv.tail(5))
+    divider_index = len(train_csv)
+    total_csv = train_csv.append(test_csv)
+    total_data = total_csv.dropna(axis=1)
+    total_data = pd.get_dummies(total_data)
+    train_data = total_data[:divider_index]
+    test_data = total_data[divider_index:]
+    print(train_csv.tail(5))
+    print(test_csv.tail(5))
+
+
+    # 데이터 정규화
+    ss = StandardScaler()
+    ss.fit(train_data)
+    train_scaled = ss.transform(train_data)
+    train_scaled = torch.tensor(train_scaled).float()
+    train_target = torch.tensor(y).float()
+
+    ss.fit(test_data)
+    test_scaled = ss.transform(test_data)
+    test_scaled = torch.tensor(test_scaled).float()
+
+    model = NeuralNetwork()
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.2)
+
+    epochs = 50000
+    hist = np.zeros(epochs)
+    for t in range(epochs):
+        logits = model(train_scaled)
+        loss = loss_fn(logits.squeeze(), train_target)
+        if t % 100 == 99:
+            print(t, loss.item())
+        hist[t] = loss.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    result = model.forward(test_scaled)
+    result = result.detach().numpy()
+    result = result.reshape(-1)
+    result = pd.DataFrame(result)
+    result = result.rename(columns={0: 'SalePrice'})
+    id = test_csv['Id']
+    result['Id'] = id
+    result.to_csv('result.csv', index=False)
