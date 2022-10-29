1163fc546b31d8a58e21a47667bb29b7018fc1e3
Sun min Lee <sunmin.lee@pbagel.com>
2022-10-29 14:45:33 +0900

    batch nomalization, dropout 적용

--- model/neural_network.py
+++ model/neural_network.py
@@ -6,14 +6,14 @@ class NeuralNetwork(nn.Module):
         super(NeuralNetwork, self).__init__()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(input_length, 64, bias=True),
+            nn.BatchNorm1d(64),
             nn.ReLU(),
-            nn.Dropout(0.1),
             nn.Linear(64, 32, bias=True),
+            nn.Dropout(p=0.2),
             nn.ReLU(),
-            nn.Dropout(0.1),
             nn.Linear(32, 16, bias=True),
+            nn.BatchNorm1d(16),
             nn.ReLU(),
-            nn.Dropout(0.1),
             nn.Linear(16, 1),
         )
 
